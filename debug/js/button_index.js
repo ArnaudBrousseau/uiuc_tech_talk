8059ba45420dda2501a8ee7b432d74f1c53f49d6
Arnaud Brousseau <arnaud.brousseau@gmail.com>
2013-09-27 23:21:44 -0700

    Adds a sample buggy JS script.

---
+++ debug/js/button_index.js
@@ -0,0 +1,8 @@
+var buttons = document.querySelectorAll('button');
+var textarea = document.querySelector('textarea');
+
+for (var i=0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function() {
+        textarea.innerText = i;
+    }, false);
+}
